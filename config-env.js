7f917351a54ba461860a97f131abb8e03df3e07f
ima1019 <academyh410@gmail.com>
2024-07-04 20:48:33 +0530

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -1,16 +1,8 @@
+const GITHUB_AUTH_TOKEN = "null"
+const BOT_NUMBER = "null"
 
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-
-const username = fs.readFileSync('github_username.txt', 'utf8').trim()
 
 module.exports = {
-    SESSION_ID:  process.env.SESSION_ID,    
-    BOT_NUMBER:  process.env.BOT_NUMBER,
-    GITHUB_USERNAME: username,
-    GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN,
-};
+GITHUB_AUTH_TOKEN,
+BOT_NUMBER
+}
